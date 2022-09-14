dd6c3ed493108fa9ccc770792de89bb1165eebe9
Matheus <matheus_sa_santos@hotmail.com>
2022-09-14 20:03:42 -0300

    adicionando css no webpack e styled components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-
+import './root.css';
 const rootNode = document.getElementById('app');
 
 if (rootNode) {
