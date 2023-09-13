1fc685974955172564ec11e7440efc6dbe76bd2f
Matheus Santos <matheus_sa_santos@hotmail.com>
2023-09-12 21:45:53 -0300

    mudando importacao

--- backend/src/modules/user/useCases/createUser/createUserUseCase.provider.ts
+++ backend/src/modules/user/useCases/createUser/createUserUseCase.provider.ts
@@ -1,5 +1,5 @@
 import { Provider } from '@nestjs/common';
-import { CreateUserUseCase } from './createUserUsecase';
+import { CreateUserUseCase } from './createUserUseCase';
 import { CREATE_USER_USE_CASE_PROVIDER } from '@src/constants';
 
 export const CreateUserUseCaseProvider: Provider = {
